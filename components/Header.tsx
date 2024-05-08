a058bb3c0be3ccbd54aaf1f99ab87aefe5787f87
Gunal <113603447+gunal-01@users.noreply.github.com>
2024-05-08 09:25:48 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,6 +34,7 @@ return (
         <header>
                 <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/20
                 rounded-b-2xl">
+                    
                     <div 
                         className="
                         absolute 
@@ -51,6 +52,13 @@ return (
                         -z-50
                         "
                     />
+                    <Image
+                src="https://links.papareact.com/c2cdd5"
+                alt="Trello Logo"
+                width={150}
+                height={50}
+                className="w=44 md:w=56 pb-10 md:pb-0 object-contain"
+                />
                 <div className="flex items-center 
                 space-x-5 flex-1 justify-end
                 w-full">
